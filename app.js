666d7bed01a3cc3a7aa7121516af86527c73f1e9
Thiru <thiru85@gmail.com>
2016-10-19 14:14:02 +0000

    Hoorahhhh

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+ 
+var app = express();
+ 
+app.get('/', function (req, res) {
+  res.send('hello world, This is developed by Thirumalai Aiyalu as a Jenkins Demo');
+});
+ 
+app.listen(process.env.PORT || 5000);
+ 
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');
+var app = require('../app.js');
+ 
+describe('GET /', function() {
+  it('respond with hello world', function(done) {
+    request(app).get('/').expect('hello world, This is developed by Thirumalai Aiyalu as a Jenkins Demo', done);
+  });
+});
