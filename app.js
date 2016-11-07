b5b80daf2665dad3898467e1f41e5b01c1613c57
Thiru <thiru85@gmail.com>
2016-11-07 07:24:22 +0000

    Reverted changes

--- app.js
+++ app.js
@@ -3,31 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-
-<h1>Rusty Steele</h1>
-<p>Hi, I'm a dog. Woof!</p>
-
-<img src="http://i.imgur.com/Zl0A6erm.jpg">
-
-<h2>Some of my favourite places:</h2>
-
-<ul>
-	<li>The beach</li>
-	<li>The dog park</li>
-	<li>The fire hydrant</li>
-</ul>
-
-<p>Make sure to follow me on <a href="http://www.instagram.com">Instagram</a></p>
-
-</body>
-</html>
-');
+res.send('THIS IS A TEST LINE OF TEXT FOR NODE.JS');
 });
  
 app.listen(process.env.PORT || 5000);
